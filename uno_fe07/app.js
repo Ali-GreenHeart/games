997f28849fa7c8ab80f3925f0695c4eb467135d2
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2022-12-12 18:25:26 +0400

    (uno) check cards on click

--- uno_fe07/app.js
+++ uno_fe07/app.js
@@ -4,7 +4,7 @@ const start = document.getElementById('start')
 const divStart = document.querySelector('.divStart')
 let zIndex = 1
 let opponentCards = []
-
+let lastCard = null;
 
 createCards(6, opponentElem)
 createCards(6, playerElem)
@@ -15,34 +15,42 @@ createCards(6, playerElem)
 function clickOnCard(e) {
     const colorMe = e.target.style.backgroundColor
     const simvolMe = e.target.firstElementChild.innerHTML
-    console.log(colorMe, simvolMe)
-    e.target.classList.add('selected')
-    e.target.style.zIndex = ++zIndex
-
-    // opponent gedish!
-    const foundObj = opponentCards.find(({ symbol, color }) => simvolMe === symbol || colorMe === color)
-    if (foundObj) {
-        const index = opponentCards.indexOf(foundObj)
-        opponentCards.splice(index, 1)
-        const newCard = document.createElement('div')
-        newCard.className = 'card style selected'
-        newCard.style.backgroundColor = foundObj.color
-        const span = document.createElement('span')
-        span.innerHTML = foundObj.symbol
-        newCard.appendChild(span)
-        newCard.style.zIndex = ++zIndex
-        playerElem.append(newCard)
-        opponentElem.firstElementChild.remove()
-    } else {
-        let newCard = document.createElement('div')
-        newCard.className = 'card cardHIDDEN'
-        opponentElem.appendChild(newCard)
-        const card_details = randomCard()
-        const symbol = card_details[0]
-        const color = card_details[1]
-        opponentCards.push({ symbol, color })
+    if (!lastCard) {
+        lastCard = { color: colorMe, symbol: simvolMe }
     }
-    console.log(foundObj)
+    if (colorMe === lastCard.color || simvolMe === lastCard.symbol) {
+        console.log(colorMe, simvolMe)
+        e.target.classList.add('selected')
+        e.target.style.zIndex = ++zIndex
+
+        // opponent gedish!
+        const foundObj = opponentCards.find(({ symbol, color }) => simvolMe === symbol || colorMe === color)
+        if (foundObj) {
+            const index = opponentCards.indexOf(foundObj)
+            opponentCards.splice(index, 1)
+            const newCard = document.createElement('div')
+            newCard.className = 'card style selected'
+            newCard.style.backgroundColor = foundObj.color
+            const span = document.createElement('span')
+            span.innerHTML = foundObj.symbol
+            lastCard = { ...foundObj }
+            newCard.appendChild(span)
+            newCard.style.zIndex = ++zIndex
+            playerElem.append(newCard)
+            opponentElem.firstElementChild.remove()
+        } else {
+            let newCard = document.createElement('div')
+            newCard.className = 'card cardHIDDEN'
+            opponentElem.appendChild(newCard)
+            const card_details = randomCard()
+            const symbol = card_details[0]
+            const color = card_details[1]
+            opponentCards.push({ symbol, color })
+        }
+        console.log(foundObj)
+    } else {
+        alert('eee duz karti gel gorum, oyun cixarma!')
+    }
 }
 
 
