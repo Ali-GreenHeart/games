10e885cca89d23c1e7e14f26020c70db289ef8a9
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2022-12-19 19:00:43 +0400

    findit - add business logic

--- fe07_Find_It/app.js
+++ fe07_Find_It/app.js
@@ -1,15 +1,40 @@
 const colorMode = document.getElementById('colorMode')
 const colorModeChangerBtn = document.getElementById('colorModeChangerBtn')
-const randomWord = document.getElementById('randomWord')
+const randomWordElem = document.getElementById('randomWordElem')
 const enteredWordInput = document.getElementById('enteredWordInput')
+const results = document.querySelector('.results')
+
+randomWordElem.textContent = WORDS[randomIndexCreator()].en
 
 enteredWordInput.onkeydown = (event) => {
     if (event.key === 'Enter') {
         checkWord()
     }
 }
-
 colorModeChangerBtn.onclick = changeColorMode // dark mode -  light mode
+
+function checkWord() {
+    const enteredWord = enteredWordInput.value;
+    const azeVersion = WORDS.find((word) => word.en === randomWordElem.textContent).az
+    const azeVersionIndex = WORDS.findIndex((word) => word.en === randomWordElem.textContent)
+
+    const p = document.createElement('p')
+    p.innerHTML = `<span>${randomWordElem.textContent}</span>`
+    if (enteredWord === azeVersion) {
+        p.innerHTML += ` - <span> ${azeVersion} </span>`
+        WORDS.splice(azeVersionIndex, 1)
+    } else {
+        p.innerHTML += ` - <span> <strike> ${enteredWord}</strike> (${azeVersion})</span>`
+    }
+    results.appendChild(p)
+    enteredWordInput.value = "" // reset input value
+    randomWordElem.textContent = WORDS[randomIndexCreator()].en
+    results.scrollTo({
+        top: results.scrollHeight
+    })
+}
+
+
 
 function changeColorMode() {
     const isDarkMode = colorMode.classList.contains('darkMode')
@@ -17,11 +42,10 @@ function changeColorMode() {
     colorMode.classList.toggle('darkMode')
 }
 
-function checkWord() {
-    const enteredWord = enteredWordInput.value;
-    console.log(enteredWord)
-
-    enteredWordInput.value = "" // reset input value
+function randomIndexCreator() {
+    const randomIndex = Math.floor(Math.random() * WORDS.length)
+    return randomIndex;
 }
 
 
+// 1. validation (can't enter space)
