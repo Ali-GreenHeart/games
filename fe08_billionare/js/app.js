66d41f80e53ad098412e8a1922921a16587cbe1b
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2022-12-22 17:21:05 +0400

    billionaire - add 50-50 btn to html

--- fe08_billionare/js/app.js
+++ fe08_billionare/js/app.js
@@ -3,15 +3,28 @@ login_btn.addEventListener('click', onLogIn)
 function randomQuestionGenerator() {
     const question = getRandomQuestion()
     questionElem.textContent = question.question
-    question.answers.forEach((answer) => {
+    fiftyFiftyBtn.onclick = () => fiftyFiftyBtnClick(question)
+    relocateIndexesOfArray(question.answers).forEach((answer) => {
         const p = document.createElement('p')
         p.textContent = answer
         p.onclick = (e) => {
-            if (e.target.textContent === question.correctAnswer) {
-                console.log('dogru cavab')
+            const dogrudurmu = e.target.textContent === question.correctAnswer
+
+            if (dogrudurmu) {
                 e.target.classList.add('correctAnswer')
-            }else{
+                setTimeout(() => {
+                    answerContainer.removeChild(answerContainer.firstElementChild)
+                    answerContainer.removeChild(answerContainer.firstElementChild)
+                    answerContainer.removeChild(answerContainer.firstElementChild)
+                    answerContainer.removeChild(answerContainer.firstElementChild)
+                    randomQuestionGenerator()
+                    answerContainer.classList.remove('disabledAnswersContainer')
+                }, 750);
+            } else {
                 e.target.classList.add('wrongAnswer')
+                fiftyFiftyBtn.disabled = 'true'
+                jokerBtn.disabled = 'true'
+                phoneBtn.disabled = 'true'
             }
             answerContainer.classList.add('disabledAnswersContainer')
         }
@@ -20,3 +33,12 @@ function randomQuestionGenerator() {
     console.log(question)
 }
 randomQuestionGenerator()
+
+
+
+
+/*
+----BUGS!----
+1. Cavab verenden sonra komekci buttonlar disabled olmalidir.
+
+*/
