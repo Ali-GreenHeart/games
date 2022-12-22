3e3d1a89aa41ff07b281d39aa771155c4a1111c4
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2022-12-22 16:47:40 +0400

    billionaire - add question logic

--- fe08_billionare/js/app.js
+++ fe08_billionare/js/app.js
@@ -1,1 +1,22 @@
 login_btn.addEventListener('click', onLogIn)
+
+function randomQuestionGenerator() {
+    const question = getRandomQuestion()
+    questionElem.textContent = question.question
+    question.answers.forEach((answer) => {
+        const p = document.createElement('p')
+        p.textContent = answer
+        p.onclick = (e) => {
+            if (e.target.textContent === question.correctAnswer) {
+                console.log('dogru cavab')
+                e.target.classList.add('correctAnswer')
+            }else{
+                e.target.classList.add('wrongAnswer')
+            }
+            answerContainer.classList.add('disabledAnswersContainer')
+        }
+        answerContainer.appendChild(p)
+    })
+    console.log(question)
+}
+randomQuestionGenerator()
