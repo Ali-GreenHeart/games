7a151be275458e6b22bfd6105f2254a32e7980af
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2022-12-22 17:20:52 +0400

    billionaire - add relocateArray and 50-50 btn click

--- fe08_billionare/js/utils.js
+++ fe08_billionare/js/utils.js
@@ -20,3 +20,32 @@ function getRandomQuestion() {
     QS.splice(questionIndex, 1)
     return question;
 }
+
+function relocateIndexesOfArray(arr) {
+    const r1 = Math.floor(Math.random() * arr.length)
+    const r2 = Math.floor(Math.random() * arr.length)
+    arraymove(arr, r1, 2)
+    arraymove(arr, r2, 3)
+    function arraymove(arr, fromIndex, toIndex) {
+        var element = arr[fromIndex];
+        arr.splice(fromIndex, 1);
+        arr.splice(toIndex, 0, element);
+    }
+    return arr;
+}
+
+
+function fiftyFiftyBtnClick(question) {
+    if (fiftyFiftyBtnClick.disabled) {
+        alert('isletmisen!')
+    } else {
+        fiftyFiftyBtn.disabled = 'true'
+        const dogru_cvb_index = question.answers.indexOf(question.correctAnswer)
+        const indexes = [0, 1, 2, 3]
+        indexes.splice(dogru_cvb_index, 1)
+        relocateIndexesOfArray(indexes)
+        answerContainer.children[indexes[0]].style.visibility = 'hidden'
+        answerContainer.children[indexes[2]].style.visibility = 'hidden'
+        console.log(indexes)
+    }
+}
