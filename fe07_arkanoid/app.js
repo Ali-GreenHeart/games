c783f98c9fc0335dc66bd5251d3b7a16799a298f
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2022-12-23 18:57:16 +0400

    arkanoid - rename CONSTs, add onLose logic

--- fe07_arkanoid/app.js
+++ fe07_arkanoid/app.js
@@ -2,6 +2,9 @@ const ball = document.getElementById("ball")
 const block = document.querySelector(".block")
 const cubeContainer = document.querySelector('.cube-container')
 
+generateCubes()
+ball_move()
+
 const COLORS = [
     'red',
     'aqua',
@@ -11,23 +14,26 @@ const COLORS = [
     'pink'
 ]
 const CUBE_COUNT = 15
-const SPEED_Y = 50
-const SPEED_X = 5
-let ballDirection = 1
-let ballSpeed = 10
+const BLOCK_SPEED = 50
+const BALL_SPEED_X = 5
+const BALL_SPEED_Y = 10
+const BALL_MOVE_INTERVAL_ID = setInterval(ball_move, 40)
 
-setInterval(ball_move, 40)
-generateCubes()
-ball_move()
+let ballDirection_x = 1
+let ballDirection_y = 1
+
 
 
 function ball_move() {
+    if (parseInt(ball.style.bottom) < 20) {
+        onLose()
+    }
     let minimum_y = 0
     let ball_left_side = parseInt(ball.style.left)
     let ball_right_side = parseInt(ball.style.left) + 40
 
-    ball.style.bottom = parseInt(ball.style.bottom) + ballSpeed + 'px'
-    ball.style.left = parseInt(ball.style.left) + (ballDirection * SPEED_X) + 'px'
+    ball.style.bottom = parseInt(ball.style.bottom) + (ballDirection_y * BALL_SPEED_Y) + 'px'
+    ball.style.left = parseInt(ball.style.left) + (ballDirection_x * BALL_SPEED_X) + 'px'
     if (parseInt(ball.style.bottom) === 32) {
         // eger top block-un sol terefine deyse
         if (parseInt(ball.style.left) >= parseInt(block.style.left)
@@ -35,7 +41,7 @@ function ball_move() {
             parseInt(ball.style.left) < parseInt(block.style.left) + 75
         ) {
             // direction menfi olsun ki, sol terefe getsin
-            ballDirection = -1
+            ballDirection_x = -1
         } else if (
             // eger top block-un sag terefine deyse
             parseInt(ball.style.left) >= parseInt(block.style.left) + 75
@@ -43,12 +49,12 @@ function ball_move() {
             parseInt(ball.style.left) <= parseInt(block.style.left) + 150
         ) {
             // direction musbet olsun ki, sag terefe getsin
-            ballDirection = 1
+            ballDirection_x = 1
         }
     }
     // sol-sag divarlara deyende istiqamet deyishsin
     if (parseInt(ball.style.left) > 640 || parseInt(ball.style.left) < 0) {
-        ballDirection = -ballDirection
+        ballDirection_x = -ballDirection_x
     }
 
 
@@ -64,18 +70,18 @@ function ball_move() {
 
     // yuxari asagi hereket 
     if (parseInt(ball.style.bottom) > 460) {
-        ballSpeed *= -1
+        ballDirection_y *= -1
     } else if (parseInt(ball.style.bottom) < minimum_y) {
-        ballSpeed = Math.abs(ballSpeed)
+        ballDirection_y = 1
     }
 }
 
 
 window.addEventListener('keydown', (e) => {
     if (e.key === 'ArrowRight') {
-        block.style.left = parseInt(block.style.left) + SPEED_Y + 'px'
+        block.style.left = parseInt(block.style.left) + BLOCK_SPEED + 'px'
     } else if (e.key === 'ArrowLeft') {
-        block.style.left = parseInt(block.style.left) - SPEED_Y + 'px'
+        block.style.left = parseInt(block.style.left) - BLOCK_SPEED + 'px'
     }
     if (parseInt(block.style.left) < 0) {
         block.style.left = '0px'
@@ -94,6 +100,16 @@ function generateCubes() {
     }
 }
 
+function onLose() {
+    clearInterval(BALL_MOVE_INTERVAL_ID)
+    console.log('uduzdun')
+    setTimeout(() => {
+        ball.style.display = 'none'
+    }, 300);
+    // todo: top partlasin, partlayanda ses cixarsin
+}
+
 function randomIndex(end) {
     return Math.floor(Math.random() * end)
 }
+
