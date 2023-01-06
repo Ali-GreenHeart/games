a022076717c58f7dd09d3e6e941597c194425c90
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-01-06 16:45:17 +0400

    mern02_sbs:

---
+++ mern02_symbol_by_symbol/app.js
@@ -0,0 +1,22 @@
+const suallar = [
+    {
+        sual: 'En boyuk gol hansidir?',
+        cavab: 'xezer'
+    },
+    {
+        sual: 'Sen harda yasayirsan?',
+        cavab: 'yer'
+    },
+    {
+        sual: 'Ali muellime hardan elcatanliq etmek olar?',
+        cavab: 'github'
+    },
+    {
+        sual: 'Ali muellime nece muraciet etmek olar?',
+        cavab: 'ali'
+    },
+    {
+        sual: 'Nicatin adi nedir?',
+        cavab: 'aybeniz'
+    },
+]
