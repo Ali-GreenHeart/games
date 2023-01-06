bf0d7dac0663069d42382b17075c4815f8330c77
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-01-06 16:54:55 +0400

    mern02_sbs:  add dynamic word_container, alphabetClick

--- mern02_symbol_by_symbol/app.js
+++ mern02_symbol_by_symbol/app.js
@@ -18,5 +18,29 @@ const suallar = [
     {
         sual: 'Nicatin adi nedir?',
         cavab: 'aybeniz'
-    },
+    }
 ]
+
+const questionElem = document.getElementById('question')
+const wordContainerElem = document.getElementById('word_container')
+const symbolsContainerElem = document.getElementById('symbols_container')
+
+
+
+let cavab = ''
+questionElem.textContent = suallar[1].sual
+let cavabUzunluq = suallar[1].cavab.length
+
+for (let i = 0; i < cavabUzunluq; i++) {
+    const newDiv = document.createElement('div')
+    newDiv.id = `div${i}`
+    wordContainerElem.appendChild(newDiv)
+}
+
+let lastIndex = 0
+
+function alphabetClick(symbol) {
+    document.getElementById(`div${lastIndex}`).textContent = symbol
+    lastIndex++
+    console.log(symbol)
+}
