b3f8169b64d64183f03013c8c771db2408cc0dcb
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-05-02 21:43:28 +0400

    (mp3) dynamic data, functionality

--- mp3_player/app.js
+++ mp3_player/app.js
@@ -8,22 +8,79 @@ const btnNextSong = document.getElementById('btnNextSong')
 const btnForward = document.getElementById('btnForward')
 
 
+let currentSong = 0
 
+const songs = [
+    {
+        id: 0,
+        name: "lion",
+        mp3Name: "lion.ogg",
+        img: "first.webp",
+        singer: "alion"
+    },
+    {
+        id: 1,
+        name: "nashkanDelamo",
+        mp3Name: "nashkanDelamo.mp3",
+        img: "qruz.jpg",
+        singer: "rahib"
+    },
+    {
+        id: 2,
+        name: "gholMidam",
+        mp3Name: "gholMidam.mp3",
+        img: "second.png",
+        singer: "balaeli"
+    },
+]
 
+writeData(currentSong)
 
 btnPlayPause.onclick = () => {
     if (song.paused) {
-        btnPlayPause.innerHTML = '<i class="fa-solid fa-pause"></i>'
-        song.play()
+        playSong()
     } else {
-        btnPlayPause.innerHTML = '<i class="fa-solid fa-play"></i>'
-        song.pause()
+        pauseSong()
     }
 }
+
 
 btnForward.onclick = () => {
     song.currentTime += 5
 }
 btnBackward.onclick = () => {
     song.currentTime -= 5
+}
+btnNextSong.onclick = () => {
+    if (currentSong >= songs.length - 1) {
+        currentSong = 0
+    } else {
+        currentSong++
+    }
+    writeData(currentSong)
+    playSong()
+}
+btnPreSong.onclick = () => {
+    if (currentSong === 0) {
+        currentSong = songs.length - 1
+    } else {
+        currentSong--
+    }
+    writeData(currentSong)
+    playSong()
+}
+
+function writeData(c) {
+    songImg.src = `./img/${songs[c].img}`
+    songName.textContent = `${songs[c].name} by ${songs[c].singer}`
+    song.src = `./mp3/${songs[c].mp3Name}`
+}
+
+function playSong() {
+    btnPlayPause.innerHTML = '<i class="fa-solid fa-pause"></i>'
+    song.play()
+}
+function pauseSong() {
+    btnPlayPause.innerHTML = '<i class="fa-solid fa-play"></i>'
+    song.pause()
 }
