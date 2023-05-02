aa63276dc1763b13c64704e4fbc6059ec656dcc3
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-05-02 21:12:38 +0400

    (mp3_player) add play/pause functionality

---
+++ mp3_player/app.js
@@ -0,0 +1,19 @@
+const song = document.getElementById('song')
+const songImg = document.getElementById('songImg')
+const songName = document.getElementById('songName')
+const btnBackward = document.getElementById('btnBackward')
+const btnPreSong = document.getElementById('btnPreSong')
+const btnPlayPause = document.getElementById('btnPlayPause')
+const btnNextSong = document.getElementById('btnNextSong')
+const btnForward = document.getElementById('btnForward')
+
+
+btnPlayPause.onclick = () => {
+    if (song.paused) {
+        btnPlayPause.innerHTML = '<i class="fa-solid fa-pause"></i>'
+        song.play()
+    } else {
+        btnPlayPause.innerHTML = '<i class="fa-solid fa-play"></i>'
+        song.pause()
+    }
+}
