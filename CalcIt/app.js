7e09e85bf37cb91b54e8a1843a6188c46f80fead
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-05-04 21:31:14 +0400

    calcit simple functionality (calculation)

--- CalcIt/app.js
+++ CalcIt/app.js
@@ -4,10 +4,16 @@ function btnForNumber(ed) {
     enteredNumber.value += ed
 }
 
+let birinciEded = 0
+let operator = ''
+let ikinciEded = 0
 
-function btnForOperator(operator) {
-
+function btnForOperator(_operator) {
+    birinciEded = +enteredNumber.value
+    enteredNumber.value = ''
+    operator = _operator
 }
+
 
 function btnForClear() {
 
@@ -18,7 +24,27 @@ function btnForDot() {
 }
 
 function btnForCalculate() {
-
+    ikinciEded = +enteredNumber.value
+    switch (operator) {
+        case '+':
+            enteredNumber.value = birinciEded + ikinciEded
+            break;
+        case '-':
+            enteredNumber.value = birinciEded - ikinciEded
+            break;
+        case '/':
+            enteredNumber.value = birinciEded / ikinciEded
+            break;
+        case 'pow2':
+            enteredNumber.value = birinciEded * birinciEded
+            break;
+        case '*':
+            enteredNumber.value = birinciEded * ikinciEded
+            break;
+        case 'sqrt':
+            enteredNumber.value = Math.sqrt(birinciEded)
+            break;
+    }
 }
 
 function btnForBackspace() {
