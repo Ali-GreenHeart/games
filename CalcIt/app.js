81a37b5e359fe44f20786601f355189f793296bd
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-05-04 21:08:44 +0400

    calcit add initial functionality (functions)

---
+++ CalcIt/app.js
@@ -0,0 +1,26 @@
+const enteredNumber = document.getElementById('entered_number')
+
+function btnForNumber(ed) {
+    console.log(ed)
+}
+
+
+function btnForOperator(operator) {
+
+}
+
+function btnForClear() {
+
+}
+
+function btnForDot() {
+
+}
+
+function btnForCalculate() {
+
+}
+
+function btnForBackspace() {
+
+}
